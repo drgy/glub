d1520021815d68ff242a4369f119cbb5de8f58cf
drgy <drgonjak@gmail.com>
2022-05-03 19:31:57 +0200

    Add gitignore

--- src/utils/logging.ts
+++
@@ -1,5 +0,0 @@
-export function time(): string {
-	let now = new Date();
-
-	return `[${now.getHours()}:${now.getMinutes()}.${now.getMilliseconds()} ${('0' + now.getDate()).slice(-2)}.${('0' + now.getMonth()).slice(-2)}.${now.getFullYear().toString().slice(-2)}] `;
-}
